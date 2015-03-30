d67ca3325a83dd896408f6092589039a8abaf313
Johannes Wachter <johannes.wachter@massiveart.at>
2015-03-30 17:41:50 +0200

    init dist files

---
+++ Resources/public/dist/main.js
@@ -0,0 +1,1 @@
+require.config({paths:{massiveactivity:"../../massiveactivity/js"}}),define(function(){"use strict";return{name:"Massive Activity Bundle",initialize:function(a){a.components.addSource("massiveactivity","/bundles/massiveactivity/js/components")}}});
